Fix number of blind-spot computation
Internally still a number of pixels was used. Now it is switched completely to a percentage of pixels.

n2v/internals/N2V_DataWrapper.py
@@ -24,7 +24,7 @@ class N2V_DataWrapper(Sequence):
                         The manipulator used for the pixel replacement.
     """
 
-    def __init__(self, X, Y, batch_size, num_pix=1, shape=(64, 64),
+    def __init__(self, X, Y, batch_size, perc_pix=0.198, shape=(64, 64),
                  value_manipulation=None):
         self.X, self.Y = X, Y
         self.batch_size = batch_size
@@ -35,16 +35,20 @@ class N2V_DataWrapper(Sequence):
         self.dims = len(shape)
         self.n_chan = X.shape[-1]
 
+        num_pix = int(np.product(shape)/100.0 * perc_pix)
+        assert num_pix >= 1, "Number of blind-spot pixels is below one. At least {}% of pixels should be replaced.".format(100.0/np.product(shape))
+        print("{} blind-spots will be generated per training patch of size {}.".format(num_pix, shape))
+
         if self.dims == 2:
             self.patch_sampler = self.__subpatch_sampling2D__
-            self.box_size = np.round(np.sqrt(shape[0] * shape[1] / num_pix)).astype(np.int)
+            self.box_size = np.round(np.sqrt(100/perc_pix)).astype(np.int)
             self.get_stratified_coords = self.__get_stratified_coords2D__
             self.rand_float = self.__rand_float_coords2D__(self.box_size)
             self.X_Batches = np.zeros([X.shape[0], shape[0], shape[1], X.shape[3]])
             self.Y_Batches = np.zeros([Y.shape[0], shape[0], shape[1], Y.shape[3]])
         elif self.dims == 3:
             self.patch_sampler = self.__subpatch_sampling3D__
-            self.box_size = np.round(np.power(shape[0] * shape[1] * shape[2] / num_pix, 1/3.0)).astype(np.int)
+            self.box_size = np.round(np.sqrt(100 / perc_pix)).astype(np.int)
             self.get_stratified_coords = self.__get_stratified_coords3D__
             self.rand_float = self.__rand_float_coords3D__(self.box_size)
             self.X_Batches = np.zeros([X.shape[0], shape[0], shape[1], shape[2], X.shape[4]])

n2v/utils/n2v_utils.py
@@ -71,32 +71,29 @@ def pm_identity(local_sub_patch_radius):
     return identity
 
 
-def manipulate_val_data(X_val, Y_val, num_pix=64, shape=(64, 64), value_manipulation=pm_uniform_withCP(5)):
+def manipulate_val_data(X_val, Y_val, perc_pix=0.198, shape=(64, 64), value_manipulation=pm_uniform_withCP(5)):
     dims = len(shape)
     if dims == 2:
-        box_size = np.round(np.sqrt(shape[0] * shape[1] / num_pix)).astype(np.int)
+        box_size = np.round(np.sqrt(100/perc_pix)).astype(np.int)
         get_stratified_coords = dw.__get_stratified_coords2D__
         rand_float = dw.__rand_float_coords2D__(box_size)
     elif dims == 3:
-        box_size = np.round(np.power(shape[0] * shape[1] * shape[2] / num_pix, 1 / 3.0)).astype(np.int)
+        box_size = np.round(np.sqrt(100/perc_pix)).astype(np.int)
         get_stratified_coords = dw.__get_stratified_coords3D__
         rand_float = dw.__rand_float_coords3D__(box_size)
 
     n_chan = X_val.shape[-1]
 
+    Y_val *= 0
     for j in tqdm(range(X_val.shape[0]), desc='Preparing validation data: '):
         coords = get_stratified_coords(rand_float, box_size=box_size,
                                             shape=np.array(X_val.shape)[1:-1])
-        y_val = []
-        x_val = []
-        for k in range(len(coords)):
-            y_val.append(np.copy(Y_val[(j, *coords[k], ...)]))
-            x_val.append(value_manipulation(X_val[j, ...], coords[k], dims))
+        for c in range(n_chan):
+            indexing = (j,) + coords + (c,)
+            indexing_mask = (j,) + coords + (c + n_chan,)
+            y_val = X_val[indexing]
+            x_val = value_manipulation(X_val[j, ..., c], coords, dims)
 
-        Y_val[j] *= 0
-
-        for k in range(len(coords)):
-            for c in range(n_chan):
-                Y_val[(j, *coords[k], c)] = y_val[k][c]
-                Y_val[(j, *coords[k], n_chan+c)] = 1
-                X_val[(j, *coords[k], c)] = x_val[k][c]
+            Y_val[indexing] = y_val
+            Y_val[indexing_mask] = 1
+            X_val[indexing] = x_val