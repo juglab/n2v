8658c9cb9b70bbdcdfe4e34ec745f93742a5edbb
Gabriella Turek <turek@mpi-cbg.de>
2020-06-18 14:34:22 +0200

    Added test and sample tzw compressed file, see Issue #44

---
+++ tests/test_read_lzw_tiff.py
@@ -0,0 +1,17 @@
+import pytest
+import tifffile
+from PIL import Image
+
+'''
+ Original non compressed file can be downloaded from https://imagej.nih.gov/ij/images/flybrain.zip
+ LZW compressed file obtained from https://online-converting.com/image/convert2tiff
+'''
+class TestLZWCompressedTiff():
+ 
+    def test_tifffile(self):
+        image_lzw = 'tests/flybrain_lzw.tiff'
+        tifffile.imread(image_lzw)
+        
+    def test_pillow(self):
+        image_lzw = 'tests/flybrain_lzw.tiff'
+        im1 = Image.open(image_lzw)
